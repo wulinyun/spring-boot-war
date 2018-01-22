36d0d3186fd51dee441de2a635bd8d4c7248efd0
dell <dell@DESKTOP-EJ8GLQD>
2018-01-22 10:19:09 +0800

    更新

--- src/main/java/com/pivotal/example/demo/controller/RestBaseController.java
+++ src/main/java/com/pivotal/example/demo/controller/RestBaseController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestBaseController {
 	@RequestMapping("/")
 	public String home(){
-		return "hello World 3333333!";
+		return "hello World 22222!";
 	}
 }
