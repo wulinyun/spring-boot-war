6fa17995fb2e1483a69e5da89efb1a310012eb58
dell <dell@DESKTOP-EJ8GLQD>
2018-03-06 10:34:53 +0800

    33333333

--- src/main/java/com/pivotal/example/demo/controller/RestBaseController.java
+++ src/main/java/com/pivotal/example/demo/controller/RestBaseController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestBaseController {
 	@RequestMapping("/")
 	public String home(){
-		return "2222222222";
+		return "3333333333";
 	}
 }
