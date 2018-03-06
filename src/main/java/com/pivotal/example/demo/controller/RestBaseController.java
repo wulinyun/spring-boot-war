aa254295786325630b780d09a54aa856d7b7e847
dell <dell@DESKTOP-EJ8GLQD>
2018-03-06 15:33:44 +0800

    88888888888

--- src/main/java/com/pivotal/example/demo/controller/RestBaseController.java
+++ src/main/java/com/pivotal/example/demo/controller/RestBaseController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestBaseController {
 	@RequestMapping("/")
 	public String home(){
-		return "77777";
+		return "8888888";
 	}
 }
