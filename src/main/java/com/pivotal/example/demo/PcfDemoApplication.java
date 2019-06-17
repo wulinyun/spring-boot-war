489c993e8e9091932f55996ade08a5fa893341fc
wulinyun <lin.wu@landasoft.com>
2019-06-17 16:38:43 +0800

    将这个spring-boot程序改为war包运行部署

--- src/main/java/com/pivotal/example/demo/PcfDemoApplication.java
+++ src/main/java/com/pivotal/example/demo/PcfDemoApplication.java
@@ -3,12 +3,14 @@ package com.pivotal.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
-public class PcfDemoApplication {
+public class PcfDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PcfDemoApplication.class, args);
